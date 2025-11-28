e1ca999c6268ba2598abb95332573a4ec89b09f4
patled2000 <patled334@gmail.com>
2025-11-28 18:04:00 +0530

    action

--- src/__mocks__/swiper/modules.js
+++
@@ -1,2 +0,0 @@
-export const Navigation = {};
-export const Pagination = {};
--- src/__mocks__/swiper/react.js
+++
@@ -1,4 +0,0 @@
-import React from "react";
-
-export const Swiper = ({ children }) => <div>{children}</div>;
-export const SwiperSlide = ({ children }) => <div>{children}</div>;
